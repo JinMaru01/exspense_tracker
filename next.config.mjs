e36cdd8cf05cad573756a1a234b837af6ecea6da
JinMaru01 <darachinkong1402@gmail.com>
2025-10-29 09:59:01 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === 'true'
+const repoName = 'exspense_tracker'
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,6 +13,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export', 
+  basePath: isGithubPages ? `/${repoName}` : '',
+  assetPrefix: isGithubPages ? `/${repoName}/` : '',
 }
 
 export default nextConfig
